1863c7b234987016ae893e73a2244af3b570e936
Jiahang Liu <jiahang.j.liu@gmail.com>
2017-11-21 16:29:29 -0600

    Testing Git4

--- app/src/main/java/com/example/jiahang/pvrm/InstructionFragment.java
+++ app/src/main/java/com/example/jiahang/pvrm/InstructionFragment.java
@@ -19,6 +19,9 @@ public class InstructionFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "whichInstruction";
 
+    // THIS IS AN EXAMPLE CHANGE
+    // TO TEST FOR GITHUB
+
     ImageView topPic;
     ImageView botPic;
     TextView instruction_one;
